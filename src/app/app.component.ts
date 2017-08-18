7efe47c6e9abe0800d9d3d52268c3e24805953bb
Darrell <dgambl00@yahoo.com>
2017-08-18 09:25:18 -0500

    August 18 Healthy Build Checkin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit, HostListener } from '@angular/core';
+// import { AppService } from './app.service';
+import { Router } from '@angular/router';
+import { MenuMock } from './shared/mockdata/menu';
+import { SearchMock } from './shared/mockdata/search';
+import { MdDialog, MdSnackBar } from '@angular/material';
 
 @Component({
   selector: 'gt-root',
