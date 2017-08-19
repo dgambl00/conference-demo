c7223beff8f37ae117c8dd340764a58999b6b0b4
Darrell <dgambl00@yahoo.com>
2017-08-19 16:48:35 -0500

    Commiting before adding bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-//Third party modules
+// Third party modules
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MaterialModule, MdTabsModule, MdMenuModule } from '@angular/material';
 import { GridModule } from '@progress/kendo-angular-grid';
 import { ChartsModule } from '@progress/kendo-angular-charts';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule, MdTabsModule, MdButtonModule, MdCardModule, MdMenuModule, MdToolbarModule, MdIconModule } from '@angular/material';
 
-//Custom Components
+// Custom Components
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 
@@ -22,7 +23,16 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ChartsModule
+    ChartsModule, 
+    GridModule,
+    MaterialModule, 
+    MdTabsModule, 
+    MdButtonModule, 
+    MdCardModule, 
+    MdMenuModule, 
+    MdToolbarModule, 
+    MdIconModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
